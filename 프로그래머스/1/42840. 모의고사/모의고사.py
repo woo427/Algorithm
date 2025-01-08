80c376416dc7645ec7efff62f5352ce65f71ccba
woo427 <annejwkim00@naver.com>
2025-01-08 14:12:34 +0900

    [level 1] Title: 모의고사, Time: 2.71 ms, Memory: 9.99 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -0,0 +1,18 @@
+def solution(answers):
+    student1 = [1,2,3,4,5]
+    student2 = [2,1,2,3,2,4,2,5]
+    student3 = [3,3,1,1,2,2,4,4,5,5]
+    scores = [0,0,0]
+    answer = []
+    
+    for i in range(len(answers)):
+        if answers[i] == student1[i%5]:
+            scores[0] += 1
+        if answers[i] == student2[i%8]:
+            scores[1] += 1
+        if answers[i] == student3[i%10]:
+            scores[2] += 1
+    for index, score in enumerate(scores):
+        if score == max(scores):
+            answer.append(index+1)
+    return answer
