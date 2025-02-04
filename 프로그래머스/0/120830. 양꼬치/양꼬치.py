9dfe79a709b49925ca90af6251158baba7b5644a
woo427 <annejwkim00@naver.com>
2025-02-04 15:37:32 +0900

    [level 0] Title: 양꼬치, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.py
@@ -0,0 +1,8 @@
+def solution(n, k):
+    answer = 0
+    if n >= 10 :
+        drink = n // 10
+        answer = 12000 * n + 2000 * (k - drink)
+    else :
+        answer = 12000 * n + 2000 * k
+    return answer
