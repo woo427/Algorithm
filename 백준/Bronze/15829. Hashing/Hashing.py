f2da1751c41770b5a485ab60b90b9ddf14fa630d
김지우 <annejwkim00@naver.com>
2025-09-09 20:08:42 +0900

    [Bronze II] Title: Hashing, Time: 36 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -4,10 +4,11 @@ n = int(sys.stdin.readline())
 str = sys.stdin.readline().strip()
 count = 0
 total = 0
+m = 1234567891
 
 for ch in str:
   num = ord(ch) - 96
   total += num * pow(31, count)
   count += 1
 
-print(total)
+print(total % m)
