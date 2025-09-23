3ec5f3553a6d16b0f6c63451ab6b1a571776be8c
김지우 <annejwkim00@naver.com>
2025-09-23 17:33:51 +0900

    [Silver V] Title: 영화감독 숌, Time: 552 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -0,0 +1,20 @@
+import sys
+
+a = int(sys.stdin.readline())
+
+num = 666
+count = 0
+
+while True:
+
+  
+
+  if '666' in str(num):
+    count += 1
+    if count == a:
+      print(num)
+      break
+  
+  num += 1
+
+  
